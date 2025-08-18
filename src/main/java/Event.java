4339ea6f17e170a288ee406a8a66654276df2dc0
ruissouw <rohit16@live.com>
2025-08-18 21:18:49 +0800

    create event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+public class Event extends Task {
+    protected String start;
+    protected String end;
 
-public class Event {
+    public Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(from:" + start + "to:" + end + ")";
+    }
 }
