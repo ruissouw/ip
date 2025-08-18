cab7da9f58094bbcbf6fa1d1073d8394cb798f66
ruissouw <rohit16@live.com>
2025-08-18 13:19:37 +0800

    change duke to penguin and add greeting

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,10 +1,15 @@
-public class Duke {
+public class Penguin {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        */
+
+        System.out.println("Hello! I'm Penguin \nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
