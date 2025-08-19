2d9d2c3f44aa19b55cd29dff96ff78845d8b1c24
ruissouw <rohit16@live.com>
2025-08-19 16:46:32 +0800

    add penguin exception

---
+++ src/main/java/PenguinException.java
@@ -0,0 +1,20 @@
+public class PenguinException extends Exception {
+    private final String errorType;
+
+    public PenguinException(String errorType) {
+        this.errorType = errorType;
+    }
+
+    @Override
+    public String toString() {
+        if (errorType.equals("todo") || errorType.equals("deadline") || errorType.equals("event")) {
+            return "____________________________________________________________\n"
+                    + errorType + " is fine but what do you want to do?\n"
+                    + "____________________________________________________________";
+        }
+        return "____________________________________________________________\n"
+                + "i'm smart but even i do not know what that means\n"
+                + "____________________________________________________________";
+    }
+}
+
