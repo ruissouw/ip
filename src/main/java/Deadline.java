07089ab558fcf72121baa8278074f454e7d16851
ruissouw <rohit16@live.com>
2025-08-28 11:14:19 +0800

    add support for datetime field

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        this.by = LocalDateTime.parse(by, formatter);
     }
 
-    public String getDeadline() {
+    public LocalDateTime getDeadline() {
         return this.by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString()
+                + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mma")) + ")";
     }
 }
 
