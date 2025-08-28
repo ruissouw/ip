6dd26d9e511450a15c66e370dc2b81672f424592
ruissouw <rohit16@live.com>
2025-08-29 01:36:36 +0800

    rewrite using tasklist class

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,14 +1,10 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.io.FileWriter;
 
 
 public class Penguin {
-    private static List<Task> tasks = new ArrayList<>();
+    private static TaskList tasks;
     private static Ui ui = new Ui();
     private static Storage storage = new Storage();
 
@@ -27,58 +23,17 @@ public class Penguin {
         }
     }
 
-    /*
-    private static void readFileContents(String filePath) throws FileNotFoundException {
-        File f = new File(filePath);
-        Scanner s = new Scanner(f);
-        while (s.hasNext()) {
-            String[] split = s.nextLine().split(" \\| ");
-            Task task = null;
-            if (split[0].equals("T")) {
-                task = new Todo(split[2]);
-            } else if (split[0].equals("D")) {
-                task = new Deadline(split[2], split[3]);
-            } else if (split[0].equals("E")) {
-                task = new Event(split[2], split[3], split[4]);
-            }
-
-            if (split[1].equals("[X]")) {
-                task.markAsDone();
-            }
-            tasks.add(task);
-        }
-    }
-
-    private static void writeAllTasks(String filePath, List<Task> tasks) throws IOException {
-        FileWriter fw = new FileWriter(filePath);
-        for (Task task : tasks) {
-            String line = null;
-            if (task instanceof Todo) {
-                line = "T | " + task.getStatusIcon() + " | " + task.getDescription();
-            } else if (task instanceof Deadline) {
-                line = "D | " + task.getStatusIcon() + " | " + task.getDescription()
-                        + " | " + ((Deadline) task).getDeadline();
-            } else if (task instanceof Event) {
-                line = "E | " + task.getStatusIcon() + " | " + task.getDescription()
-                        + " | " + ((Event) task).getStart() + " | " + ((Event) task).getEnd();
-            }
-            fw.write(line + System.lineSeparator());
-        }
-        fw.close();
-    }
-     */
-
     public static void main(String[] args) {
         try {
-            storage.readFileContents(tasks);
+            tasks = new TaskList(storage.load());
         } catch (FileNotFoundException e) {
-            // new File("./data").mkdirs();
             storage.createFolder();
             try {
-                // new File(FILE_PATH).createNewFile();
                 storage.createFile();
+                tasks = new TaskList(new ArrayList<>());
             } catch (IOException io) {
                 ui.printErrorMessage("Unable to create file: " + io.getMessage());
+                tasks = new TaskList(new ArrayList<>());
             }
         }
 
@@ -88,32 +43,31 @@ public class Penguin {
             String str = ui.readLine();
             if (str.equals("bye")) {
                 try {
-                    storage.writeAllTasks(tasks);
+                    storage.writeAllTasks(tasks.getTasks());
                 } catch (IOException e) {
                     ui.printErrorMessage(e.getMessage());
                 }
                 ui.sayGoodbye();
                 break;
             } else if (str.equals("list")) {
-                ui.printList(tasks);
+                ui.printList(tasks.getTasks());
             } else if (str.contains("mark") || str.contains("unmark")) {
                 String[] split = str.split(" ");
                 int idx = Integer.parseInt(split[1]) - 1;
-                Task task = tasks.get(idx);
 
                 String msg = "";
                 if (split[0].equals("mark")) {
-                    task.markAsDone();
-                    ui.markTask(task);
+                    tasks.markAsDone(idx);
+                    ui.markTask(tasks.getTask(idx));
                 } else if (split[0].equals("unmark")) {
-                    task.markAsUndone();
-                    ui.unmarkTask(task);
+                    tasks.markAsUndone(idx);
+                    ui.unmarkTask(tasks.getTask(idx));
                 }
             } else if (str.contains("delete")) {
                 String[] split = str.split(" ");
                 int idx = Integer.parseInt(split[1]) - 1;
-                Task task = tasks.remove(idx);
-                ui.deleteTask(tasks.size(), task);
+                Task task = tasks.deleteTask(idx);
+                ui.deleteTask(tasks.getNumOfTasks(), task);
             } else {
                 try {
                     String[] split = str.split(" ", 2);
@@ -124,7 +78,7 @@ public class Penguin {
                         if (split.length == 1 || split[1].trim().isEmpty()) {
                             throw new PenguinException("todo");
                         }
-                        tasks.add(new Todo(split[1].trim()));
+                        tasks.addTask(new Todo(split[1].trim()));
                         break;
 
                     case DEADLINE:
@@ -132,7 +86,7 @@ public class Penguin {
                             throw new PenguinException("deadline");
                         }
                         String[] split2 = split[1].split("/by", 2);
-                        tasks.add(new Deadline(split2[0].trim(), split2[1].trim()));
+                        tasks.addTask(new Deadline(split2[0].trim(), split2[1].trim()));
                         break;
 
                     case EVENT:
@@ -141,10 +95,10 @@ public class Penguin {
                         }
                         String[] split3 = split[1].split("/from", 2);
                         String[] split4 = split3[1].split("/to", 2);
-                        tasks.add(new Event(split3[0].trim(), split4[0].trim(), split4[1].trim()));
+                        tasks.addTask(new Event(split3[0].trim(), split4[0].trim(), split4[1].trim()));
                         break;
                     }
-                    ui.addTask(tasks.size(), tasks.get(tasks.size() - 1));
+                    ui.addTask(tasks.getNumOfTasks(), tasks.getTask(tasks.getNumOfTasks() - 1));
                 } catch (PenguinException pe) {
                     ui.printErrorMessage(pe.toString());
                 }
