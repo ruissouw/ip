8eff89cb07401775d3be1003bd1d82959eb11d75
ruissouw <rohit16@live.com>
2025-08-29 15:36:34 +0800

    create test cases for task class

---
+++ src/test/java/penguin/TaskTest.java
@@ -0,0 +1,21 @@
+package penguin;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void testGetDescription() {
+        Task task = new Task("testing task");
+        assertEquals("testing task", task.getDescription());
+    }
+
+    @Test
+    public void testGetStatusIcon() {
+        Task task = new Task("testing task");
+        assertEquals("[ ]", task.getStatusIcon());
+        task.markAsDone();
+        assertEquals("[X]", task.getStatusIcon());
+        task.markAsUndone();
+        assertEquals("[ ]", task.getStatusIcon());
+    }
+}
