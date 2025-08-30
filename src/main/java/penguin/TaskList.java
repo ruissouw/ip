acb09b40a15aafc3e91c0a0650dfb0cd87bf7808
ruissouw <rohit16@live.com>
2025-08-30 11:36:51 +0800

    add header comments for tasklist

--- src/main/java/penguin/TaskList.java
+++ src/main/java/penguin/TaskList.java
@@ -2,6 +2,9 @@ package penguin;
 
 import java.util.List;
 
+/**
+ * The class that keeps track of the tasks in the list and performs operations
+ */
 public class TaskList {
     private List<Task> tasks;
 
@@ -9,30 +12,57 @@ public class TaskList {
         this.tasks = tasks;
     }
 
+    /**
+     * Useful for pretty printing all tasks in the list
+     * @return all tasks in the list
+     */
     public List<Task> getTasks() {
         return this.tasks;
     }
 
+    /**
+     * @return Number of tasks in the list
+     */
     public int getNumOfTasks() {
         return this.tasks.size();
     }
 
+    /**
+     * @param idx index of the task in the list
+     * @return task at specified index
+     */
     public Task getTask(int idx) {
         return this.tasks.get(idx);
     }
 
+    /**
+     * adds task to the list
+     * @param task task to be added to the list
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * deletes task at specified index
+     * @param idx index of task to be deleted to the list
+     */
     public Task deleteTask(int idx) {
         return tasks.remove(idx);
     }
 
+    /**
+     * marks task at specified index as done
+     * @param idx index of task in list
+     */
     public void markAsDone(int idx) {
         tasks.get(idx).markAsDone();
     }
 
+    /**
+     * marks task at specified index as undone
+     * @param idx index of task in list
+     */
     public void markAsUndone(int idx) {
         tasks.get(idx).markAsUndone();
     }
