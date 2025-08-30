979bddb2feec49dbf2da9087c93d9ee6f0ec8bac
ruissouw <rohit16@live.com>
2025-08-30 11:13:17 +0800

    add header comments

--- src/main/java/penguin/Event.java
+++ src/main/java/penguin/Event.java
@@ -3,6 +3,9 @@ package penguin;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Event tasks have both a start and end datetime field
+ */
 public class Event extends Task {
     protected LocalDateTime start;
     protected LocalDateTime end;
@@ -14,11 +17,19 @@ public class Event extends Task {
         this.end = LocalDateTime.parse(end, formatter);
     }
 
+    /**
+     * Returns the start datetime
+     * @return string formatted datetime
+     */
     public String getStart() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
         return this.start.format(formatter);
     }
 
+    /**
+     * Returns the end datetime
+     * @return string formatted datetime
+     */
     public String getEnd() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
         return this.end.format(formatter);
