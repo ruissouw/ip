188fbafe31e737f96a16ed7b801354e4f4afe3a3
ruissouw <rohit16@live.com>
2025-08-30 10:30:25 +0800

    add line break

--- src/main/java/penguin/PenguinException.java
+++ src/main/java/penguin/PenguinException.java
@@ -12,11 +12,11 @@ public class PenguinException extends Exception {
         if (errorType.equals("todo") || errorType.equals("deadline") || errorType.equals("event")) {
             return "____________________________________________________________\n"
                     + errorType + " is fine but what do you want to do?\n"
-                    + "____________________________________________________________";
+                        + "____________________________________________________________";
         }
         return "____________________________________________________________\n"
                 + "i'm smart but even i do not know what that means\n"
-                + "____________________________________________________________";
+                    + "____________________________________________________________";
     }
 }
 
