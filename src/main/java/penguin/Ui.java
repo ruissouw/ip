46276fcdb6f8aa1d4bb7908c0bbd1b55e5f1f764
ruissouw <rohit16@live.com>
2025-08-30 10:16:57 +0800

    add header commands for ui class

--- src/main/java/penguin/Ui.java
+++ src/main/java/penguin/Ui.java
@@ -3,6 +3,9 @@ package penguin;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * The class in charge of dealing with user interactions
+ */
 public class Ui {
     private Scanner scanner;
 
@@ -10,18 +13,31 @@ public class Ui {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Reads the next line of user input
+     * @return User Input
+     */
     public String readLine() {
         return scanner.nextLine();
     }
 
+    /**
+     * Closes the scanner
+     */
     public void close() {
         scanner.close();
     }
 
+    /**
+     * Pretty prints the border lines
+     */
     public void printLine() {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Greets the user when the app starts
+     */
     public void sayWelcome() {
         printLine();
         System.out.println(" Hello! I'm Penguin");
@@ -29,12 +45,19 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Says goodbye to the user when the app closes
+     */
     public void sayGoodbye() {
         printLine();
         System.out.println(" Bye. Hope to see you again soon!");
         printLine();
     }
 
+    /**
+     * Pretty prints all tasks in the list
+     * @param tasks Tasks in the list
+     */
     public void printList(List<Task> tasks) {
         printLine();
         System.out.println("Here are the tasks in your list:");
@@ -45,6 +68,10 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Notifies the user that the task has been marked
+     * @param task Task to be marked
+     */
     public void markTask(Task task) {
         printLine();
         System.out.println("Nice! I've marked this task as done:");
@@ -52,6 +79,10 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Notifies the user that the task has been unmarked
+     * @param task Task to be unmarked
+     */
     public void unmarkTask(Task task) {
         printLine();
         System.out.println("OK, I've marked this task as not done yet:");
@@ -59,6 +90,11 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Notifies the user that the task has been deleted
+     * @param task Task to be deleted
+     * @param totalTasks Number of tasks left in the list
+     */
     public void deleteTask(int totalTasks, Task task) {
         printLine();
         System.out.println("Noted. I've removed this task:");
@@ -67,6 +103,11 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Notifies user that the task has been added
+     * @param totalTasks New number of tasks in the list
+     * @param task Task to be added
+     */
     public void addTask(int totalTasks, Task task) {
         printLine();
         System.out.println("Got it. I've added this task:");
@@ -75,6 +116,10 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Notifies user of error
+     * @param msg Error messsage to be printed
+     */
     public void printErrorMessage(String msg) {
         System.out.println(msg);
     }
